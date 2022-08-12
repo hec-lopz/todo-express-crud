4525795f4c9d7f513c7fc1ce387bf91e975c9fdc
Hector Lopez <hlopezcarballo@gmail.com>
2022-08-12 14:46:15 -0500

    Fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express from 'express'
 import morgan from 'morgan'
-import cors from 'cors'
+import cors, { CorsOptions } from 'cors'
 import indexRoutes from './routes/index.routes'
 import './database'
 import { ORIGIN } from './config'
 
 const app = express()
+
+const whitelist = ['http://localhost', 'https://heclopz-todo-app.netlify.app']
+const options: CorsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido'))
+    }
+  },
+}
 
 // Middlewares
 app.use(cors({ origin: ORIGIN }))
